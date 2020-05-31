9b962965b0f31ebec871171be10288c2326514d9
Yifeng Zhu <yifeng.zhu@utexas.edu>
2020-05-31 02:37:14 -0500

    Added style check using cpplint.

--- scripts/prepush.sh
+++ scripts/prepush.sh
@@ -3,4 +3,12 @@ pushd `git rev-parse --show-toplevel`
 make -j`nproc`
 res=$?
 popd
+if [ $res -ne 0 ]; then
+    echo -e "\n\n\n\n"
+    echo "Build failed!"
+    exit $res
+fi
+
+scripts/lint_diff.sh
+res=$?
 exit $res
