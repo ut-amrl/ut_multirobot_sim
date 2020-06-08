0e8486c681d9c2705bb8cda54b921e69d56cd93d
Joydeep Biswas <joydeepb@cs.utexas.edu>
2020-06-08 12:12:17 -0500

    Models are named by types, not robot names

--- config/default_init_config.lua
+++ config/default_init_config.lua
@@ -1,4 +1,5 @@
-map_name =  "maps/GDC3/GDC3.txt"
+-- map_name =  "maps/Joydeepb-Home/Joydeepb-Home.vectormap.txt"
+map_name =  "maps/Joydeepb-Driveway/Joydeepb-Driveway.vectormap.txt"
 -- Simulator starting location.
 start_x = 33.016267
 start_y = 21.534157
--- src/simulator/omnidirectional_model.h
+++ src/simulator/omnidirectional_model.h
@@ -6,12 +6,12 @@
 #include "ros/ros.h"
 #include "simulator/robot_model.h"
 
-#ifndef SRC_SIMULATOR_COBOT_MODEL_H_
-#define SRC_SIMULATOR_COBOT_MODEL_H_
+#ifndef SRC_SIMULATOR_OMNIDIRECTIONAL_MODEL_H_
+#define SRC_SIMULATOR_OMNIDIRECTIONAL_MODEL_H_
 
-namespace cobot {
+namespace omnidrive {
 
-class CobotModel : public robot_model::RobotModel {
+class OmnidirectionalModel : public robot_model::RobotModel {
  private:
   ut_multirobot_sim::CobotDriveMsg last_cmd_;
   double t_last_cmd_;
@@ -25,15 +25,16 @@ class CobotModel : public robot_model::RobotModel {
   void DriveCallback(const ut_multirobot_sim::CobotDriveMsg& msg);
 
  public:
-  CobotModel() = delete;
+  OmnidirectionalModel() = delete;
   // Intialize a default object reading from a file
-  CobotModel(const std::vector<std::string>& config_files, ros::NodeHandle* n);
-  ~CobotModel() = default;
+  OmnidirectionalModel(
+      const std::vector<std::string>& config_files, ros::NodeHandle* n);
+  ~OmnidirectionalModel() = default;
   // define Step function for updating
   void Step(const double& dt);
   void PublishOdom(const float dt);
 };
 
-}  // namespace cobot
+}  // namespace omnidrive
 
-#endif  // SRC_SIMULATOR_COBOT_MODEL_H_
+#endif  // SRC_SIMULATOR_OMNIDIRECTIONAL_MODEL_H_
