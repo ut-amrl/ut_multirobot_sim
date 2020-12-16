918934f369fc714f02cc4ad7bc17a760207fc970
jarrettHoltz <random.jmh@gmail.com>
2020-12-16 13:55:48 -0600

    Adjusting evaluation scenarios (thinner hallway)

--- scenario_generator/make_scenarios.py
+++ scenario_generator/make_scenarios.py
@@ -2,6 +2,7 @@
 
 from math import pi
 import os
+import sys
 
 # Format: (x, y, theta)
 starting_positions = {
@@ -10,32 +11,37 @@ starting_positions = {
 }
 
 ending_positions = {
-    'left': { 'right': (47, 4.5, 0), 'up': (21.5, 9, pi/2) },
-    'right': { 'left': (3, 4.5, pi), 'up': (21.5, 9, pi/2)}
+    'left': { 'right': (43, 4.5, 0), 'up': (16, 7, pi/2) },
+    'right': { 'left': (3, 4.5, pi), 'up': (16, 7, pi/2)}
 }
 
-human_counts = (0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
+human_counts = (2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
 
-scenarios_per_hc = 20
+scenarios_per_hc = 5
 
-for start_pos_name in starting_positions:
-    start_x, start_y, start_angle = starting_positions[start_pos_name]
-    ending_pos_names = ending_positions[start_pos_name].keys()
-    for ending_pos_name in ending_positions[start_pos_name]:
-        goal_x, goal_y, goal_angle = ending_positions[start_pos_name][ending_pos_name]
-        for human_count in human_counts:
-            for i in range(scenarios_per_hc):
-                scenario_name = f"sp.{start_pos_name}_ep.{ending_pos_name}_hc.{human_count}_{i}"
-                command = "python3 scenario_generator.py " \
-                    f"--out ../scenarios/{scenario_name} " \
-                    f"--scenario_name {scenario_name} " \
-                    f"--count {human_count} " \
-                    f"--start_x {start_x} " \
-                    f"--start_y {start_y} " \
-                    f"--start_angle {start_angle} " \
-                    f"--goal_x {goal_x} " \
-                    f"--goal_y {goal_y} " \
-                    f"--goal_angle {goal_angle} "
-                print(scenario_name)
-                print(command)
-                os.system(command)
+def main():
+    scenarios_per_hc = int(sys.argv[1])
+    for start_pos_name in starting_positions:
+        start_x, start_y, start_angle = starting_positions[start_pos_name]
+        ending_pos_names = ending_positions[start_pos_name].keys()
+        for ending_pos_name in ending_positions[start_pos_name]:
+            goal_x, goal_y, goal_angle = ending_positions[start_pos_name][ending_pos_name]
+            for human_count in human_counts:
+                for i in range(scenarios_per_hc):
+                    scenario_name = f"sp.{start_pos_name}_ep.{ending_pos_name}_hc.{human_count}_{i}"
+                    command = "python3 scenario_generator.py " \
+                        f"--out ../scenarios/{scenario_name} " \
+                        f"--scenario_name {scenario_name} " \
+                        f"--count {human_count} " \
+                        f"--start_x {start_x} " \
+                        f"--start_y {start_y} " \
+                        f"--start_angle {start_angle} " \
+                        f"--goal_x {goal_x} " \
+                        f"--goal_y {goal_y} " \
+                        f"--goal_angle {goal_angle} "
+                    print(scenario_name)
+                    print(command)
+                    os.system(command)
+
+if __name__ == '__main__':
+    main()
--- scenario_generator/scenario_generator.py
+++ scenario_generator/scenario_generator.py
@@ -52,11 +52,11 @@ def make_scenario(dir_name: str, config: dict) -> None:
         if not os.path.exists(dir_name + '/humans'):
             os.mkdir(dir_name + '/humans')
         position_x = random.uniform(5, 35)
-        position_y = 1 if random.choice((True, False)) else 5
+        position_y = 1.5 if random.choice((True, False)) else 3.0
         human_positions.append((position_x, position_y))
-        if (position_y == 1):
+        if (position_y == 1.5):
             human_positions_lower.append((position_x, position_y))
-        if (position_y == 5):
+        if (position_y == 3.0):
             human_positions_upper.append((position_x, position_y))
         #  waypoints = [(position_x, position_y)]
 
