58cba6e52a78cfbefaf87fedc9710054c3137ab3
jarrettHoltz <random.jmh@gmail.com>
2020-12-22 14:48:12 -0600

    Updating sim setup

--- config/ut_jackal_config.lua
+++ config/ut_jackal_config.lua
@@ -3,12 +3,12 @@ require("config.sim_config");
 -- MODEL PARAMETERS
 invert_linear_vel_cmds = false
 invert_angular_vel_cmds = false
-linear_pos_accel_limit = 3.0
-linear_neg_accel_limit = 3.0
-angular_pos_accel_limit = 3.0
-angular_neg_accel_limit = 3.0
+linear_pos_accel_limit = 6.0
+linear_neg_accel_limit = 6.0
+angular_pos_accel_limit = 6.0
+angular_neg_accel_limit = 6.0
 max_angular = 3.0
-max_linear_vel = 6.0
+max_linear_vel = 10.0
 drive_callback_topic = "/navigation/cmd_vel"
 -- drive_callback_topic = "/pedbot/control/cmd_vel"
 diff_drive_odom_topic = "/jackal_velocity_controller/odom"
